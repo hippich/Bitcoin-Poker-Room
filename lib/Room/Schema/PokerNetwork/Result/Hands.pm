7b732ee1f6393952bd639c7ed7be4992b37e433b
Pavel Karoukin <pavel@yepcorp.com>
2010-09-06 19:46:21 -0500

    added correct licensing description.

--- lib/Room/Schema/PokerNetwork/Result/Hands.pm
+++ lib/Room/Schema/PokerNetwork/Result/Hands.pm
@@ -46,4 +46,28 @@ __PACKAGE__->set_primary_key("serial");
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
+
+=head1 AUTHOR
+
+Pavel Karoukin
+
+=head1 LICENSE
+
+Copyright (C) 2010 Pavel A. Karoukin <pavel@yepcorp.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
+
 1;
--- lib/Room/Schema/PokerNetwork/Result/TourneysSchedule2prizes.pm
+++ lib/Room/Schema/PokerNetwork/Result/TourneysSchedule2prizes.pm
@@ -26,4 +26,28 @@ __PACKAGE__->add_columns(
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
+
+=head1 AUTHOR
+
+Pavel Karoukin
+
+=head1 LICENSE
+
+Copyright (C) 2010 Pavel A. Karoukin <pavel@yepcorp.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
+
 1;
