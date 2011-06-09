d90a9b6983ac12043c32597a42560bf865f87f39
anisoptera <github@lotuswind.net>
2011-06-08 23:05:26 -0700

    fix Ax/Ad typo

--- lib/Room/Schema/PokerNetwork/Result/Hands.pm
+++ lib/Room/Schema/PokerNetwork/Result/Hands.pm
@@ -47,7 +47,7 @@ __PACKAGE__->set_primary_key("serial");
 use JSON::XS;
 use Switch;
 
-our @poker_cards_string = ( '2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', 'Th', 'Jh', 'Qh', 'Kh', 'Ah', '2d', '3d', '4d', '5d', '6d', '7d', '8d', '9d', 'Td', 'Jd', 'Qd', 'Kd', 'Ax', '2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', 'Tc', 'Jc', 'Qc', 'Kc', 'Ac', '2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s', 'Ts', 'Js', 'Qs', 'Ks', 'As' );
+our @poker_cards_string = ( '2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', 'Th', 'Jh', 'Qh', 'Kh', 'Ah', '2d', '3d', '4d', '5d', '6d', '7d', '8d', '9d', 'Td', 'Jd', 'Qd', 'Kd', 'Ad', '2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', 'Tc', 'Jc', 'Qc', 'Kc', 'Ac', '2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s', 'Ts', 'Js', 'Qs', 'Ks', 'As' );
 
 __PACKAGE__->has_many(
   'userhands' => 'Room::Schema::PokerNetwork::Result::User2hand',
