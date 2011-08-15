f95cacae51238ef67ceda8319627642ce0e90ff6
Pavel Karoukin <pavel@karoukin.us>
2011-08-15 01:08:11 -0500

    Refactor default templates to allow custom theming.

--- root/static/script.js
+++ root/static/script.js
@@ -71,3 +71,49 @@ var attach_behaviors = function(c) {
 function popitup(link, game_id) {
   jQuery('<a rel="table" target="table_' + game_id + '" href="'+ link +'" />').popupwindow(table_profile).click();
 }
+
+jQuery(document).ready(function() { 
+  jQuery(".tweet").tweet({
+      username: "betcoin",
+      join_text: null,
+      avatar_size: null,/*AVATAR*/
+      count: 1,/*NUMBER OF TWEETS*/
+      auto_join_text_default: "we said,", 
+      auto_join_text_ed: "we",
+      auto_join_text_ing: "we were",
+      auto_join_text_reply: "we replied to",
+      auto_join_text_url: "we were checking out",
+      loading_text: "loading tweets..."
+  });
+});
+
+jQuery(document).ready(function() {
+  jQuery.ajax({
+    url: 'http://betcoin.blogspot.com/feeds/posts/default?alt=json-in-script',
+    dataType: 'jsonp',
+    success: function (data) {
+      var entries = data.feed.entry;
+      jQuery("#blogspot-posts").html('<ul></ul>');
+
+      var len = entries.length;
+      if (len > 2) { 
+        len = 2;
+      }
+      
+      for (var i=0; i < entries.length; i++) {
+        var date = new Date(entries[i].published["$t"]);
+        var date_str = date.format("mmmm d, yyyy");
+        var href = entries[i].link[4].href;
+        var title = entries[i].link[4].title;
+        var comments_href = entries[i].link[1].href;
+        var comments_title = entries[i].link[1].title;
+
+        jQuery("#blogspot-posts ul").append(
+          "<li><a href='" + href + "' target='_blank'>" + title + "</a>" +
+          "<p>" + date_str + ", <a href='" + comments_href + "' target='_blank'>" +
+          comments_title + "</a></p></li>" 
+        );
+      }
+    }
+  });
+})
